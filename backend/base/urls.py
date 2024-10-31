0a6e4d93bf95cee2843c5ae04a0ec2f4a18a63e6
CitizenFourCF4 <mitya.ulyanov97@mail.ru>
2024-10-31 22:48:11 +0300

    Сделал рефакторинг бэкенда

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,20 @@
+"""
+This module contains the URL routing configuration for the application.
+It maps URL patterns to corresponding views, allowing the application
+to respond to HTTP requests with the appropriate view functions or classes.
+"""
+
 from django.urls import path
-from .views import get_chat_info, user_chats_list_view, ChatEventHandlerView, create_message_view
+from .views import (
+    ChatEventHandlerView,
+    create_message_view,
+    get_chat_info_view,
+    user_chats_list_view
+    )
 
 urlpatterns = [
-    path("user_chats/", user_chats_list_view, name="chats"),
-    path("upgrade_chat/", ChatEventHandlerView.as_view(), name="upgrade_chat"),
-    path('chat/<str:pk>/', get_chat_info, name='chat'),
     path('add_message/', create_message_view, name='add_message'),
+    path('chat/<str:pk>/', get_chat_info_view, name='chat'),
+    path("upgrade_chat/", ChatEventHandlerView.as_view(), name="upgrade_chat"),
+    path("user_chats/", user_chats_list_view, name="chats"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,178 +1,292 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.contrib.auth.models import User
-from .models import Chat, Pdf2FileMessage, Text2Speech
-from django.core.files.storage import FileSystemStorage
+"""
+This module contains views for processing HTTP requests
+in the application. Views define the logic for processing requests and
+return appropriate responses.
+"""
 
-from rest_framework.decorators import api_view
-from rest_framework import status
-
-from backend.settings import MEDIA_URL, MEDIA_ROOT
 import os
 
 from dotenv import load_dotenv
+from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
+from pydantic import BaseModel
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from backend.settings import MEDIA_URL, MEDIA_ROOT
 from model_integration import process_pdf_2_file, process_text_to_speech
+from .models import Chat, Pdf2FileMessage, Text2Speech
 
 load_dotenv()
 
 @api_view(['GET'])
-def user_chats_list_view(request):
-   username = request.query_params.get('username')
-   # если пользователь только зарегистрировался в системе, то возникает случай User.create()
-   user, _ = User.objects.get_or_create(username=username)
-   user_chats_list = [
-      {
-        'chat_id': chat.id,
-        'chat_title': chat.title,
-        'chat_mode': chat.mode,
-        'created_at': chat.created_at,
-      } 
-      for chat in Chat.objects.filter(user=user)]
-   
-   data = {
+def user_chats_list_view(request:Request)->Response:
+    """Function for obtaining a list of user chats upon request from the client side
+
+    Args:
+        request (Request): GET request from the client side
+
+    Returns:
+        Response: List of chats in which the user is a member
+    """
+    username = request.query_params.get('username')
+    # if the user has just registered in the system, then the case User.create() occurs
+    user, _ = User.objects.get_or_create(username=username)
+    user_chats_list = [
+        {
+            'chat_id': chat.id,
+            'chat_title': chat.title,
+            'chat_mode': chat.mode,
+            'created_at': chat.created_at,
+        }
+    for chat in Chat.objects.filter(user=user)]
+
+    data = {
       'user_chats': user_chats_list[::-1]
-   }
-   return Response(data)
-  
+    }
+    return Response(data, status=status.HTTP_200_OK)
+
 
 @api_view(['GET'])
-def get_chat_info(request, pk):
-   chat = Chat.objects.get(id=pk)
+def get_chat_info_view(request:Request, pk:int)->Response:
+    """Returns chat information
 
-   if chat.mode == 'Extract PDF text':
-      messages = Pdf2FileMessage.objects.filter(chat=pk)
-   # messages = Message.objects.filter(chat=pk)
-      output = {
-         "chat_title": chat.title,
-         "chat_mode": chat.mode,
-         "messages": [
-            {
-            'message': obj.message,
-            'author': obj.user.username,
-            'message_type': obj.message_type,
-            'filename': obj.filename,
-            'filesize': obj.filesize,
-            'created_at': obj.created_at,
-            }
-            for obj in messages]
-      }
+    Args:
+        request (Request): GET request from the client side
+        pk (int): chat id
 
-   if chat.mode == 'Text to speech':
-      messages = Text2Speech.objects.filter(chat=pk)
-      output = {
-         "chat_mode": chat.mode,
-         "chat_title": chat.title,
-         "messages": [
+    Returns:
+        Response: Returns chat information: 
+        - chat_title: str
+        - chat_mode: str
+        - messages: list
+    """
+    chat = Chat.objects.get(id=pk)
+
+    if chat.mode == 'Extract PDF text':
+        messages = Pdf2FileMessage.objects.filter(chat=pk)
+        messages = [
             {
-            'message': obj.message,
-            'author': obj.user.username,
-            'message_type': obj.message_type,
-            'filename': obj.filename,
-            'filesize': obj.filesize,
-            'created_at': obj.created_at,
+            'message': msg.message,
+            'author': msg.user.username,
+            'message_type': msg.message_type,
+            'filename': msg.filename,
+            'filesize': msg.filesize,
+            'created_at': msg.created_at,
             }
-            for obj in messages]
-      }
-   return Response(output)
-    
-  
+        for msg in messages]
+    elif chat.mode == 'Text to speech':
+        messages = Text2Speech.objects.filter(chat=pk)
+        messages = [
+            {
+            'message': msg.message,
+            'author': msg.user.username,
+            'message_type': msg.message_type,
+            'created_at': msg.created_at,
+            }
+        for msg in messages]
+    else:
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
+    output = {
+        "chat_title": chat.title,
+        "chat_mode": chat.mode,
+        "messages": messages
+    }
+    return Response(output, status=status.HTTP_200_OK)
+
+
 
 class ChatEventHandlerView(APIView):
-   def post(self, request): # создание чата
-      chat_title = request.data['chat_title']
-      chat_mode = request.data['chat_mode']
-      username = request.data['username']
-      user = User.objects.get(username=username)
+    """This class provides event handlers related to chat. 
+    It includes a handlers for creating a chat, deleting a chat, and changing the chat title
 
-      new_object = Chat.objects.create(
-            title = chat_title,
-            mode = chat_mode,
+    Args:
+        APIView
+    """
+    class PostRequestValidator(BaseModel):
+        """Post request validator
+
+        Args:
+            BaseModel
+        """
+        chat_title:str
+        chat_mode:str
+        username:str
+
+    class PutRequestValidator(BaseModel):
+        """Put request validator
+
+        Args:
+            BaseModel
+        """
+        chat_id:str
+        new_title:str
+
+
+    def post(self, request:Request)->Response:
+        """Chat creation handler
+
+        Args:
+            request (Request): POST request from the client side
+
+        Returns:
+            Response: request status code
+        """
+        try:
+            request_data = self.PostRequestValidator.model_validate(request.data)
+        except:
+            return Response(status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        user = User.objects.get(username=request_data.username)
+        new_object = Chat.objects.create(
+            title = request_data.chat_title,
+            mode = request_data.chat_mode,
             user = user,
         )
-      id = new_object.id
-      new_object.href = f'chat/{id}'
-      new_object.save()
-      return Response(status=status.HTTP_201_CREATED) 
-   
+        id = new_object.id
+        new_object.href = f'chat/{id}'
+        new_object.save()
+        return Response(status=status.HTTP_201_CREATED)
 
-   def put(self,request): # изменение названия чата
-      chat_instance = Chat.objects.get(id=request.data['chat_id'])
-      chat_instance.title = request.data['new_title']
-      chat_instance.save()
-      return Response({}) 
+    def put(self, request:Request)->Response:
+        """Chat title changer handler
+
+        Args:
+            request (Request): PUT request from the client side
+
+        Returns:
+            Response: request status code
+        """
+        try:
+            request_data = self.PutRequestValidator.model_validate(request.data)
+        except:
+            return Response(status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        try:
+            chat_instance = Chat.objects.get(id=request_data.chat_id)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            chat_instance.title = request_data.new_title
+            chat_instance.save()
+        except:
+            return Response(status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        return Response(status=status.HTTP_200_OK)
+
+    def delete(self, request:Request)->Response:
+        """Chat delete handler
+
+        Args:
+            request (Request): DELETE request from the client side
+
+        Returns:
+            Response: request status code
+        """
+
+        try:
+            chat_instance = Chat.objects.filter(id=request.data['chat_id'])
+            chat_instance.delete()
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return Response(status=status.HTTP_200_OK)
 
 
-   def delete(self, request): # удаление чата
-      chat = Chat.objects.filter(id=request.data['chat_id'])
-      chat.delete()
-      return Response(status=status.HTTP_200_OK) 
+
+class CreateMessageValidator(BaseModel):
+    chat_id:int
+    username:str
+    message:str
+    message_type:str
 
 
 @api_view(['POST'])
-def create_message_view(request):
-   
-   chat_id = int(request.data['chat_id'])
-   username = request.data['username']
-   message = request.data['message']
-   message_type = request.data['message_type']
+def create_message_view(request:Request)->Response:
+    """Chat message creation handler
 
-   if message_type == 'file':
-      fs = FileSystemStorage()
-      filename = fs.save(message.name, message)
+    Args:
+        request (Request): POST request from the client side
 
-   chat = Chat.objects.get(id=chat_id)
+    Returns:
+        Response: _description_
+    """
+    try:
+        request_data = CreateMessageValidator.model_validate(request.data)
+    except:
+        return Response(status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-   if chat.mode == 'Extract PDF text':
-      link = os.path.join(os.getenv('SERVER_URL'),os.path.join(MEDIA_URL, filename).lstrip('/'))
-      Pdf2FileMessage.objects.create(
-         chat=chat,
-         user=User.objects.get(username=username),
-         message=link,
-         message_type = 'file',
-         filename = filename,
-         filesize=message.size
-      )
+    try:
+        user = User.objects.get(username=request_data.username)
+    except:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-      answer = process_pdf_2_file(os.path.join(MEDIA_ROOT, filename), MEDIA_ROOT)
-      print(answer)
-      user_chatbot, _ = User.objects.get_or_create(username='chatbot')
-      markdown_filename = filename.removesuffix('.pdf') + '.mmd'
-      link = os.path.join(os.getenv('SERVER_URL'),os.path.join(MEDIA_URL, markdown_filename).lstrip('/'))
-      try:
-         filesize = os.path.getsize(os.path.join(MEDIA_ROOT, markdown_filename))
-      except:
-         filesize = 'Unknown'
-      Pdf2FileMessage.objects.create(
-         chat=chat,
-         user = user_chatbot,
-         message=link,
-         message_type='file',
-         filename=markdown_filename,
-         filesize=filesize
-      )
+    user_chatbot, _ = User.objects.get_or_create(username='chatbot')
 
-   elif chat.mode == 'Text to speech': 
-      Text2Speech.objects.create(
-         chat=chat,
-         user=User.objects.get(username=username),
-         message=message,
-         message_type=message_type
-      )
-      user_chatbot, _ = User.objects.get_or_create(username='chatbot')
-      filename = process_text_to_speech(message, MEDIA_ROOT)
-      link = os.path.join(os.getenv('SERVER_URL'),os.path.join(MEDIA_URL, filename).lstrip('/')) 
-      try:
-         filesize = os.path.getsize(os.path.join(MEDIA_ROOT, filename))
-      except:
-         filesize = 'Unknown'
-      Text2Speech.objects.create(
-         chat=chat,
-         user=user_chatbot,
-         message=link,
-         message_type='audio',
-         filename=filename,
-         filesize=filesize
-      )
-      
+    try:
+        chat = Chat.objects.get(id=request_data.chat_id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-   return Response({})
+    if chat.mode == 'Extract PDF text':
+        # file URL
+        if request_data.message_type == 'file':
+        # save the file in the media/
+            fs = FileSystemStorage()
+            filename = fs.save(request_data.message.name, request_data.message)
+        else:
+            return Response(status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        
+        url = os.path.join(os.getenv('SERVER_URL'),os.path.join(MEDIA_URL, filename).lstrip('/')) 
+
+        Pdf2FileMessage.objects.create(
+            chat=chat,
+            user=user,
+            message=url,
+            message_type=request_data.message_type,
+            filename=filename,
+            filesize=request_data.message.size
+        )
+
+        is_error = process_pdf_2_file(os.path.join(MEDIA_ROOT, filename), MEDIA_ROOT).returncode
+        if is_error:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        markdown_filename = filename.removesuffix('.pdf') + '.mmd'
+        # file URL
+        url = os.path.join(os.getenv('SERVER_URL'),os.path.join(MEDIA_URL, markdown_filename).lstrip('/')) 
+        try:
+            filesize = os.path.getsize(os.path.join(MEDIA_ROOT, markdown_filename))
+        except:
+            filesize = 'Unknown'
+        Pdf2FileMessage.objects.create(
+            chat=chat,
+            user = user_chatbot,
+            message=url,
+            message_type='file',
+            filename=markdown_filename,
+            filesize=filesize
+        )
+
+    elif chat.mode == 'Text to speech': 
+        Text2Speech.objects.create(
+            chat=chat,
+            user=user,
+            message=request_data.message,
+            message_type=request_data.message_type
+        )
+        filename = process_text_to_speech(request_data.message, MEDIA_ROOT)
+        link = os.path.join(os.getenv('SERVER_URL'),os.path.join(MEDIA_URL, filename).lstrip('/')) # file URL
+        Text2Speech.objects.create(
+            chat=chat,
+            user=user_chatbot,
+            message=link,
+            message_type='audio',
+        )
+    else:
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
+    return Response(status=status.HTTP_200_OK)
